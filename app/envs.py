04ed129abd2193c3601b814c3147d02529d34a5a
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-28 01:42:26 +0900

    add mysql

---
+++ app/envs.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+env = Env()
+
+DB_TYPE = env("mysql")
+DB_HOST = env("DB_HOST")
+DB_NAME = env("DB_NAME")
+DB_USER = env("DB_USER")
+DB_PASSWORD = env("DB_PASSWORD")
+DB_PORT = env("DB_PORT")
