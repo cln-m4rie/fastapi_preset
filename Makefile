4d2bc0f32e77136e632192396a7010a849535969
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-27 21:52:44 +0900

    update

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+default: help
+
 format:  ## format all python codes
 	autoflake --in-place --remove-unused-variables --remove-all-unused-imports --ignore-init-module-imports --recursive . && \
 	isort -rc && \
@@ -5,3 +7,6 @@ format:  ## format all python codes
 
 dev:
 	uvicorn app.server:app --reload
+
+help:  ## Show all of tasks
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- app/server.py
+++ app/server.py
@@ -1,8 +1,22 @@
+import logging
+
 from fastapi import FastAPI
 
 app = FastAPI()
+logger = logging.getLogger("app")
+
+handler1 = logging.StreamHandler()
+handler1.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
+
+handler2 = logging.FileHandler(filename=f"logs/app.log")  # handler2はファイル出力
+handler2.setLevel(logging.WARN)  # handler2はLevel.WARN以上
+handler2.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
+
+logger.addHandler(handler1)
+logger.addHandler(handler2)
 
 
 @app.get("/")  # methodとendpointの指定
 async def hello():
+    logger.debug("test")
     return {"text": "hello world!"}
