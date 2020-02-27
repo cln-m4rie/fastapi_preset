0cd429d1448e855389925034e31fe2f33b833ac0
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-28 00:53:11 +0900

    wip

--- app/server.py
+++ app/server.py
@@ -9,9 +9,11 @@ app = FastAPI()
 app.include_router(user.router)
 logger = logging.getLogger("app")
 
+
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
+
 file_handler = logging.FileHandler(filename=str(PathConfig.log / "app.log"))
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s %(message)s"))
