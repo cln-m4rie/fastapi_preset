3ba08276fe218f46c77cf249570e0c0907f972f9
cln-m4rie <s.kikkawa0613@gmail.com>
2020-06-07 13:56:24 +0900

    add gitignore

--- app/envs.py
+++
@@ -1,9 +0,0 @@
-from environs import Env
-
-env = Env()
-
-DB_HOST = env("DB_HOST")
-DB_NAME = env("DB_NAME")
-DB_USER = env("DB_USER")
-DB_PASSWORD = env("DB_PASSWORD")
-DB_PORT = env("DB_PORT")
--- app/routes/user.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/user")
-async def get_user():
-    return {"name": "test user"}
