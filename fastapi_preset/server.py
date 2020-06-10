c82d2f5a9bb379f40d60054012226dbf6e44fc9b
cln-m4rie <s.kikkawa0613@gmail.com>
2020-06-10 22:31:49 +0900

    add health check

--- fastapi_preset/server.py
+++ fastapi_preset/server.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+
+@app.get("/health")
+def health_check() -> JSONResponse:
+    return JSONResponse(content={"msg": "ok"}, status_code=200)
